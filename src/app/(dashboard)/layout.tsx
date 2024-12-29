7d5deb6ee5ba6d5c22bcba8169d106676ef30675
Nimira43 <n.rabone72@aol.com>
2024-12-29 23:34:09 +0000

    Starting to work on the menu items

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Menu from '@/components/Menu';
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -8,11 +9,12 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className='h-screen flex'>
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[#c1e2ef]">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[#c1e2ef] p-4">
         <Link href='/' className='flex items-center justify-center lg:justify-start gap-2'>
           <Image src='/logo.png' alt='logo' width={32} height={32} />
           <span className='logo hidden lg:block'>Xué</span>
         </Link>
+        <Menu />
       </div>
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#d8ebf2]">Right Side</div>
     </div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,37 +12,180 @@ export default Menu
 
 
 
-// const menuItems = [
-//   {
-//     title: 'Menu',
-//     items: [
-//       { 
-//         icon: '/home.png',
-//         label: 'Home',
-//         href: '/',
-//         visibile: [
-//           'admin',
-//           'teacher',
-//           'student',
-//           'parent',
-//         ],
-//       },
-//     ],
-//   },
-//   {
-//     title: 'OTHER',
-//     items: [
-//       {
-//         icon: '/profile.png',
-//         label: 'Profile',
-//         href: '/profile',
-//         visibile: [
-//           'admin',
-//           'teacher',
-//           'student',
-//           'parent',
-//         ],
-//       },
-//     ],
-//   },
-// ]
+const menuItems = [
+  {
+    title: 'Menu',
+    items: [
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+      { 
+        icon: '/home.png',
+        label: 'Home',
+        href: '/',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+    ],
+  },
+  {
+    title: 'OTHER',
+    items: [
+      {
+        icon: '/profile.png',
+        label: 'Profile',
+        href: '/profile',
+        visibile: [
+          'admin',
+          'teacher',
+          'student',
+          'parent',
+        ],
+      },
+    ],
+  },
+]
