91717c0cb5e9ad77a1d3c4b3a33486a396c19c91
Nimira43 <n.rabone72@aol.com>
2024-12-29 23:03:13 +0000

    Set up logo inside Link to /

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,7 +9,11 @@ export default function DashboardLayout({
   return (
     <html lang='en'>
       <div className='h-screen flex'>
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[#c1e2ef]">left</div>
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[#c1e2ef]">
+          <Link href='/'>
+            <Image src='/logo.png' alt='logo' width={32} height={32} />
+          </Link>
+        </div>
         
         <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#d8ebf2]">right</div>
       </div>
