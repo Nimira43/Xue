21225b5740ccccb1d89357730baba83513315f50
Nimira43 <n.rabone72@aol.com>
2024-12-29 23:14:46 +0000

    Setting up size and positioning of logo and logo text.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,16 +7,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <div className='h-screen flex'>
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[#c1e2ef]">
-          <Link href='/'>
-            <Image src='/logo.png' alt='logo' width={32} height={32} />
-          </Link>
-        </div>
-        
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#d8ebf2]">right</div>
+    <div className='h-screen flex'>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[#c1e2ef]">
+        <Link href='/' className='flex items-center justify-center lg:justify-start gap-2'>
+          <Image src='/logo.png' alt='logo' width={32} height={32} />
+          <span className='logo hidden lg:block'>Xué</span>
+        </Link>
       </div>
-    </html>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#d8ebf2]">Right Side</div>
+    </div>
   )
 }
