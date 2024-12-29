bdbf2871caf71a23a11e939116a24d79a66272ca
Nimira43 <n.rabone72@aol.com>
2024-12-29 22:39:05 +0000

    ...and for xl screens

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,9 +6,9 @@ export default function DashboardLayout({
   return (
     <html lang='en'>
       <div className='h-screen flex'>
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] bg-[#c1e2ef]">left</div>
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[#c1e2ef]">left</div>
         
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] bg-[#d8ebf2]">right</div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#d8ebf2]">right</div>
       </div>
     </html>
   )
