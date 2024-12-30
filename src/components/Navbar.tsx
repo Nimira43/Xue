a696a0f8064bf5f51c32915c85b17257fef18bfa
Nimira43 <n.rabone72@aol.com>
2024-12-30 02:26:43 +0000

    Tidied up user icon and added alert bubble for the announcement icon.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,14 +11,15 @@ const Navbar = () => {
         <div className='bg-[#fff] rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
           <Image src='/message.png' alt='' width={20} height={20} />
         </div>
-        <div className='bg-[#fff] rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+        <div className='bg-[#fff] rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative'>
           <Image src='/announcement.png' alt='' width={20} height={20} />
+          <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-[#87ceeb] text-[#fff] rounded-full text-xs'>1</div>
         </div>
         <div className='flex flex-col'>
           <span className='text-xs leading-3 font-medium'>Ed Walker</span>
           <span className='text-[10px] text-[#333] text-right'>Admin</span>
         </div>
-        <Image src='/user.png' alt='' width={36} height={36}  />
+        <Image src='/user.png' alt='' width={36} height={36} className='rounded-full bg-[#fff] p-1' /> {/* added p-1 */}
       </div>
     </div>
   )
