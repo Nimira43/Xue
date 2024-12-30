4adefd2a421f8fd012f86305a6431c96c127fbd8
Nimira43 <n.rabone72@aol.com>
2024-12-30 04:41:43 +0000

    Testing these new colours and fixing typos

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,7 +1,7 @@
 const UserCard = ({type}:{type:string}) => {
   return (
-    <div className='rounded-2xl odd:bg-darkSky even:bg=darkerSky'>
-      
+    <div className='rounded-2xl odd:bg-darkSky even:bg-darkerSky p4'>
+      test
     </div>
   )
 }
