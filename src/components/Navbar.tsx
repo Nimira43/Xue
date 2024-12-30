ef026442f91df3c9ade9b3a141f24919a97b1f31
Nimira43 <n.rabone72@aol.com>
2024-12-30 01:27:32 +0000

    Added searchbar, message icon and announcement icon to navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='flex items-center justify-between p-4'>
+      <div className='hidden md:flex'>
+        <Image src='/search.png' alt='' width={14} height={14} />
+        <input type='text' placeholder='Search...' />
+      </div>
+      <div className='flex items-center gap-6'>
+        <div className='bg-[#fff] rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+          <Image src='/message.png' alt='' width={20} height={20} />
+        </div>
+        <div className='bg-[#fff] rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+          <Image src='/announcement.png' alt='' width={20} height={20} />
+        </div>
+      </div>
     </div>
   )
 }
