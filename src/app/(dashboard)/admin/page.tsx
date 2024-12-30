10f23f4563421dd376f0a30b964e7c8b7dfd2abf
Nimira43 <n.rabone72@aol.com>
2024-12-30 03:13:39 +0000

    Setting up layout for Admin page. Created UserCard.tsx in components folder.

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,12 @@
 const AdminPage = () => {
   return (
-    <div>
-      Admin Page
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      <div className='w-full lg:w-2/3'>
+        Left
+      </div>
+      <div className='w-full lg:w-1/3'>
+        Right
+      </div>
     </div>
   )
 }
