ad0b99c2f2e0588b01bec0db4008e43afe9bf45f
Nimira43 <n.rabone72@aol.com>
2024-12-30 01:11:56 +0000

    Added Navbarcomponent and {children} to the main dashboard part of the layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from '@/components/Menu'
+import Navbar from '@/components/Navbar';
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -16,7 +17,10 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f3f8fa]'>Right Side</div>
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f3f8fa] overflow-scroll'>
+        <Navbar />
+        {children}
+      </div>
     </div>
   )
 }
