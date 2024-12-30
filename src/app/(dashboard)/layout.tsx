20d099cd55573bcf8546e2b4609caa7c7eec430b
Nimira43 <n.rabone72@aol.com>
2024-12-30 01:07:59 +0000

    Useding very pale blue for main dashboard area - #f3f8fa

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]'>Right Side</div>
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f3f8fa]'>Right Side</div>
     </div>
   )
 }
