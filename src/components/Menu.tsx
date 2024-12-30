a01b2a106f572861741b9297a87ec10cce30b8ee
Nimira43 <n.rabone72@aol.com>
2024-12-30 01:03:25 +0000

    Just added a few more styles - hidding menu text on small screens

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,18 +5,18 @@ import Link from "next/link"
 const Menu = () => {
   return (
     <div className='mt-4 text-sm'>
-      {menuItems.map(i => (
+      {menuItems.map((i) => (
         <div className='flex flex-col gap-2' key={i.title}>
           <span 
             className='hidden lg:block text-[#333] font-light my-4'
           >
             {i.title}
           </span>
-          {i.items.map(item => (
+          {i.items.map((item) => (
             <Link 
               href={item.href} 
               key={item.label}
-              className='flex items-center justify-center lg:justify-start gap-4 text-[#333]'
+              className='flex items-center justify-center lg:justify-start gap-4 text-[#333] py-2'
             >
               <Image 
                 src={item.icon} 
@@ -24,7 +24,7 @@ const Menu = () => {
                 width={20} 
                 height={20} 
               />
-              <span>{item.label}</span>
+              <span className='hidden lg:block'>{item.label}</span>
             </Link>
           ))}
         </div>
