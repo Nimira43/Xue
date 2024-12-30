fecb90400a5287a24fb0442c6326b9350f481482
Nimira43 <n.rabone72@aol.com>
2024-12-30 05:01:41 +0000

    Continuing styling the UserCards.

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 const UserCard = ({type}:{type:string}) => {
   return (
-    <div className='rounded-2xl odd:bg-darkSky even:bg-darkerSky p-4 flex-1'>
+    <div className='rounded-2xl odd:bg-darkSky even:bg-darkerSky p-4 flex-1 min-w-[130px]'>
       <div className='flex justify-between items-center'>
-        <span>2024/25</span>
+        <span className='text-[10px] text-[#333] font-medium bg-[#fff] px-2 py-1 rounded-full'>2024/25</span>
         <Image src='/more.png' alt='' width={20} height={20}/> 
       </div>
       <h1>1,643</h1>
